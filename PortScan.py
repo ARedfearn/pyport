6f0860e25503a1816ab7aad10b459e43c8eb742a
Alex Redfearn <alex@goodlpay.com>
2020-03-27 12:07:15 +0000

    check if address is in use

--- PortScan.py
+++ PortScan.py
@@ -1,6 +1,14 @@
 import socket
 
-def portscan():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 8080))
-    s.close()
+def portscan(port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', port))
+        s.close()
+    except Exception, e:
+        if 'Address already in use' in e:
+            print('exception message:', e)
+
+
+for i in range(8080, 8082):
+    portscan(i)
